30d158941f7187936b63e6f8c06288dd51469b75
Andrea_Abundis <lizbethabundisp@gmail.com>
2018-11-15 01:05:01 -0600

    ingreso de imagen

--- settings.php
+++ settings.php
@@ -7,6 +7,38 @@
     $rfc=$_POST['rfc'];
     $email=$_POST['email'];
     $pass=$_POST['password'];
+    
+    $tamano = $_FILES["archivo"]['size'];
+    $tipo = $_FILES["archivo"]['type'];
+    $archivo = $_FILES["archivo"]['name'];
+    $prefijo = substr(md5(uniqid(rand())),0,6);
+    if ($archivo != "") {
+        // guardamos el archivo a la carpeta files, si no existe la tienes q crear antes de subir los archivos.
+        $destino =  "files/".$prefijo."_".$archivo;
+        if (copy($_FILES['archivo']['tmp_name'],$destino)) {
+            $status = "Archivo subido con exito: <b>".$archivo."</b><br>";
+        } 
+        else {
+            $status = "Error al subir el archivo";
+        }
+      } 
+      else {
+        $status = "Error al subir archivo";}
+echo $status;
+$dir="files";
+$dh=opendir($dir);
+echo "<br>";
+echo "<table width='50' border='1'>";
+echo "<tr><td colspan='2' align='center'><b>Listado de Archivos<b></td></tr>";
+while (($file = readdir($dh)) !== false) {
+    echo "<tr>";
+      echo "<td><a href=$dir/$file>$file</a></td>";
+      echo "<td><img src='$dir/$file' width='38' height='31' border='3'></td>";
+    echo "</tr>";
+      }
+echo "</table>";
+closedir($dh);
+
     $consulta="UPDATE cliente
      set nombre = '$name',
      apellido = '',
@@ -15,7 +47,8 @@
      rfc = '$rfc',
      email = '$email',
      password = md5('$pass'),
-     empresa = ''
+     empresa = '',
+     imagen = '$archivo'
      WHERE id_cliente = 1";
     $resultado=mysqli_query($con,$consulta);
     if (!$resultado){
@@ -48,7 +81,7 @@
           <div class="login-wrapper-settings">
             <div class="login-form-wrapper">
               <div class="form-wrapper">
-                <form id="settings" action="#" method="POST">
+                <form id="settings" action="#" method="POST" enctype="multipart/form-data">
                   <label>SETTINGS</label>
                   <hr>
                   <input type="text" name="name" placeholder="Name"/>
@@ -57,6 +90,7 @@
                   <input type="text" name="rfc" placeholder="RFC"/>
                   <input type="email" name="email" placeholder="Email"/>
                   <input type="password" name="password" placeholder="Password"/>
+                  <input name="archivo" type="file" size="40"  />
                   <input type="submit" name="settings" placeholder="ENVIAR"/>
                 </form>
               </div>
