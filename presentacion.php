821ef9a1c9d0188b40e57e58bdfe10fe4d6882a7
Inigo98 <navarretigo12@gmail.com>
2018-11-28 18:11:24 -0600

    presentacion1

--- presentacion.php
+++ presentacion.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>signup</title>
+    <title>Presentacion</title>
     <link href="./styles/main.css" rel="stylesheet">
   </head>
   <!--<div id="carga">
@@ -13,9 +13,12 @@
 
     <div class="ia-workspace">
         <?php include("navbar.php"); ?>
-          <div class="login-wrapper">
-            <div class="login-form-wrapper privacy">
+          <div class="presentacion">
+            <div class="presentacion_rapper">
+              <iframe src="https://liveceti-my.sharepoint.com/personal/a17310214_live_ceti_mx/_layouts/15/Doc.aspx?sourcedoc={8c952002-5d58-4a3c-b40b-b10b87e3c313}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="1186px" height="691px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
+              <a href="presentacion.pptx" download="presentacion.pptx">PRESENTACION MEDIA QUERIES</a>
 
+              <a href="proyecto.pptx" download="proyecto.pptx">PRESENTACION PROYECTO</a>
             </div>
           </div>
         <?php include("footer.php"); ?>
