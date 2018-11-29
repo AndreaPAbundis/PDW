b57f20408354468784dea6bcbae1648f189b9f95
inigo98 <navarretigo12@gmail.com>
2018-11-29 00:10:25 -0600

    presentacion

--- presentacion.php
+++ presentacion.php
@@ -17,7 +17,7 @@
             <div class="presentacion_rapper">
               <iframe src="https://liveceti-my.sharepoint.com/personal/a17310214_live_ceti_mx/_layouts/15/Doc.aspx?sourcedoc={8c952002-5d58-4a3c-b40b-b10b87e3c313}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="1186px" height="691px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
               <a href="presentacion.pptx" download="presentacion.pptx">PRESENTACION MEDIA QUERIES</a>
-
+              <iframe src="https://liveceti-my.sharepoint.com/personal/a17310214_live_ceti_mx/_layouts/15/Doc.aspx?sourcedoc={ef170dba-7ad5-4675-b456-44df10f90085}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="1186px" height="691px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
               <a href="proyecto.pptx" download="proyecto.pptx">PRESENTACION PROYECTO</a>
             </div>
           </div>
